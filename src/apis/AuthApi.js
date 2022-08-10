6ab114543a560c1e846b77dbb78f2636470ba0e3
gengminy <kls1998@naver.com>
2022-08-11 02:06:06 +0900

    :hammer: fix(dev): api 주소 설정 변경

--- src/apis/AuthApi.js
+++ src/apis/AuthApi.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 const AuthApi = {
   requestJoin: async body => {
-    const data = await axios.post('/auth/join', body);
+    const data = await axios.post('https://api.hifihifi.site/auth/join', body);
 
     console.log(data);
     return data;
   },
   requestLogin: async body => {
-    const data = await axios.post('/auth/login', body);
+    const data = await axios.post('https://api.hifihifi.site/auth/login', body);
 
     console.log(data);
     return data;
