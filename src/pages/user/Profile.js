46de161029fc3489afa7d36ab3e28620d8faf5a6
pxxnxx <76719828+pxxnxx@users.noreply.github.com>
2022-08-16 00:37:40 +0900

    🚀 feat(profile): 일단 테스트 폼만 생성.. 폼 많이 고쳐야 함. 어렵다 #11

--- src/pages/user/Profile.js
+++ src/pages/user/Profile.js
@@ -5,11 +5,16 @@ import ProfileDto from '../../components/ProfileDto';
 
 function Profile() {
   const [profile, setProfile] = useState(null);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState('');
+  const [anonymous, setAnonymous] = useState('');
+
   const config = {
     headers: {
       Authorization:
         `Bearer ` +
-        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJoaWZpIiwiaWF0IjoxNjYwMzEwNDEyLCJzdWIiOiIxIiwicm9sZSI6IlJPTEVfVVNFUiIsImV4cCI6MTY2MDMxMjIxMn0.BcIWk1WQndFKvvtykzS-Zv6uEVnJCqtuqPC8RaO1F3q1ovZdENq9srWTm1gxjc2_QCLWyAOgg6WVCyIDdiXjlw',
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJoaWZpIiwiaWF0IjoxNjYwNTc3MjY5LCJzdWIiOiIxIiwicm9sZSI6IlJPTEVfVVNFUiIsImV4cCI6MTY2MDU3OTA2OX0._bAy1JE2fUybV2R8mn4fdS_dWiSbHPomtWodgk_AH47HQX0FuTWlcJnhDPT6KRkF6uz9O7oRqXsggjUOU4zQug',
     },
   };
   const onClick = async () => {
@@ -23,6 +28,26 @@ function Profile() {
       console.log(e);
     }
   };
+  const nameHandler = e => {
+    e.preventDefault();
+    setProfile(e.target.value);
+  };
+  const descriptionHandler = e => {
+    e.preventDefault();
+    setDescription(e.target.value);
+  };
+  const imageHandler = e => {
+    e.preventDefault();
+    setImage(e.target.value);
+  };
+  const anonymousHandler = e => {
+    e.preventDefault();
+    setAnonymous(e.target.value);
+  };
+  const submitHandler = e => {
+    e.preventDefault();
+    const profileDto = { name, description, image, anonymous };
+  };
   return (
     <div>
       <NavBar />
@@ -35,6 +60,45 @@ function Profile() {
           readOnly={true}
         />
       )}
+      <form onSubmit={submitHandler}>
+        <label for="name">이름</label>
+        <input
+          id="name"
+          type="name"
+          value={name}
+          onChange={nameHandler}
+          placeholder="이름"
+        />
+        <br />
+        <label for="description">소개</label>
+        <input
+          id="description"
+          type="description"
+          value={description}
+          onChange={descriptionHandler}
+          placeholder="소개"
+        />
+        <br />
+        <label for="image">프로필 사진</label>
+        <input
+          id="image"
+          type="link"
+          value={image}
+          onChange={imageHandler}
+          placeholder="이미지"
+        />
+        <br />
+        <label for="anonymous">비공개 여부</label>
+        <input
+          id="anonymous"
+          type="anonymous"
+          value={anonymous}
+          onChange={anonymousHandler}
+          placeholder="비공개 여부"
+        />
+        <br />
+        <button type="submit">프로필 변경</button>
+      </form>
     </div>
   );
 }
