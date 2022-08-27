a754cda63fa8ac852f0bb559fc030a2a044ad71d
squirMM <jsm6616@naver.com>
2022-08-28 02:12:17 +0900

    :rocket: feat(issue): 로그인 안해도 스토어 디테일 페이지 보이게 하고 리뷰 입력시 로그인으로.. #21

--- src/pages/store/StoreDetail.js
+++ src/pages/store/StoreDetail.js
@@ -9,34 +9,35 @@ import { Typography } from 'antd';
 
 const { Title } = Typography;
 const StoreDetail = () => {
-	const [cookies] = useCookies(['accessToken']);
+  const [cookies] = useCookies(['accessToken']);
 
-	const [_store, set_store] = useState([]);
-	const storeId = useParams().storeId;
+  const [_store, set_store] = useState([]);
+  const storeId = useParams().storeId;
 
-	useEffect(() => {
-		getStoreData();
-	}, []);
+  useEffect(() => {
+    getStoreData();
+  }, []);
 
-	const getStoreData = async () => {
-		const data = await StoreApi.requestStore(storeId, cookies.accessToken);
-		set_store(data);
-	};
+  const getStoreData = async () => {
+    const data = await StoreApi.requestStore(storeId, cookies.accessToken);
+    set_store(data);
+  };
+  console.log(_store);
 
-	return (
-		<div>
-			<Map storeInfo={_store.store} />
-			<div style={{ width: '85%', margin: '3rem auto' }}>
-				<Title level={4}> Store info </Title>
-				<hr />
-				<Reviews
-					refreshFunction={getStoreData}
-					newReview={_store.newReview}
-					ReviewList={_store.reviews}
-					postId={storeId}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <Map storeInfo={_store.store} />
+      <div style={{ width: '85%', margin: '3rem auto' }}>
+        <Title level={4}> Store info </Title>
+        <hr />
+        <Reviews
+          refreshFunction={getStoreData}
+          newReview={_store.newReview}
+          ReviewList={_store.reviews}
+          postId={storeId}
+        />
+      </div>
+    </div>
+  );
 };
 export default StoreDetail;
